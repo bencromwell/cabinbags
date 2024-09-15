da064b6c556c5edb44c3c31e92bfe59c347c0493
Ben Cromwell <ben@bencromwell.com>
2024-09-15 11:31:35 +0100

    feat: include limits in output

--- cabinbag/bags.go
+++ cabinbag/bags.go
@@ -1,6 +1,10 @@
 package cabinbag
 
-import "github.com/fatih/color"
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
 
 type CabinBag struct {
 	Name string `json:"name"`
@@ -11,6 +15,10 @@ type CabinBag struct {
 
 func (c CabinBag) FitsIn(limit *CabinBag) bool {
 	return c.X <= limit.X && c.Y <= limit.Y && c.Z <= limit.Z
+}
+
+func (c CabinBag) Ref() string {
+	return fmt.Sprintf("%s (%dx%dx%d)", c.Name, c.X, c.Y, c.Z)
 }
 
 func CheckBag(airline Airline, bag CabinBag) {
@@ -27,14 +35,14 @@ func CheckBag(airline Airline, bag CabinBag) {
 		}
 
 		if bag.FitsIn(limit) {
-			color.Green("  %s fits within %s %s limit", bag.Name, limit.Name, airline.Name)
+			color.Green("  %s fits within %s %s limit", bag.Ref(), limit.Ref(), airline.Name)
 			fitFound = true
 		} else {
-			color.Yellow("  %s does not fit within %s %s limit", bag.Name, limit.Name, airline.Name)
+			color.Yellow("  %s does not fit within %s %s limit", bag.Ref(), limit.Ref(), airline.Name)
 		}
 	}
 
 	if !fitFound {
-		color.Red("  %s does not fit within %s limits", bag.Name, airline.Name)
+		color.Red("  %s does not fit within %s limits", bag.Ref(), airline.Name)
 	}
 }
